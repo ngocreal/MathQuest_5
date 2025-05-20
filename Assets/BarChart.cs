b8cff39789439d3d736b6dd0259a7e57c5111fce
NgocChione <buithituyetngoc2402@gmail.com>
2025-05-21 02:58:28 +0700

    logic and UI leaderboard, statistic

---
+++ Assets/BarChart.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BarChart : MonoBehaviour
+{
+    [SerializeField] private GameObject barPrefab;
+
+    void Start()
+    {
+        // Mock dữ liệu
+        PlayerPrefs.SetInt("Level1_Correct", 4);
+        PlayerPrefs.SetInt("Level1_Incorrect", 2);
+        PlayerPrefs.SetInt("Level2_Correct", 8);
+        PlayerPrefs.SetInt("Level2_Incorrect", 3);
+        PlayerPrefs.SetInt("Level3_Correct", 12);
+        PlayerPrefs.SetInt("Level3_Incorrect", 5);
+        PlayerPrefs.Save();
+
+        UpdateChart();
+    }
+
+    void UpdateChart()
+    {
+        string[] levels = { "Level1", "Level2", "Level3" };
+        for (int i = 0; i < levels.Length; i++)
+        {
+            int correct = PlayerPrefs.GetInt(levels[i] + "_Correct", 0);
+            int incorrect = PlayerPrefs.GetInt(levels[i] + "_Incorrect", 0);
+
+            // Cột đúng
+            GameObject correctBar = Instantiate(barPrefab, transform);
+            correctBar.GetComponent<Image>().color = Color.green;
+            correctBar.GetComponent<RectTransform>().sizeDelta = new Vector2(20, correct * 10);
+            correctBar.GetComponent<RectTransform>().localPosition = new Vector3(i * 60 - 10, correct * 5, 0);
+
+            // Cột sai
+            GameObject incorrectBar = Instantiate(barPrefab, transform);
+            incorrectBar.GetComponent<Image>().color = Color.red;
+            incorrectBar.GetComponent<RectTransform>().sizeDelta = new Vector2(20, incorrect * 10);
+            incorrectBar.GetComponent<RectTransform>().localPosition = new Vector3(i * 60 + 10, incorrect * 5, 0);
+        }
+    }
+}
---
+++ Assets/Leaderboard.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] private GameObject rowPrefab;
+    [SerializeField] private Transform rowContainer;
+    [SerializeField] private TextMeshProUGUI playerRankText;
+
+    void Start()
+    {
+        // Mock dữ liệu
+        (string name, int score)[] topPlayers = {
+            ("Player A", 100),
+            ("Player B", 80),
+            ("Player C", 60),
+            ("Player D", 40),
+            ("Player E", 20)
+        };
+
+        int currentPlayerScore = 50; // Mock điểm người chơi hiện tại
+        DisplayLeaderboard(topPlayers, currentPlayerScore);
+    }
+
+    void DisplayLeaderboard((string name, int score)[] topPlayers, int currentPlayerScore)
+    {
+        // Tạo danh sách top 5
+        for (int i = 0; i < topPlayers.Length; i++)
+        {
+            GameObject row = Instantiate(rowPrefab, rowContainer);
+            TextMeshProUGUI[] texts = row.GetComponentsInChildren<TextMeshProUGUI>();
+            texts[0].text = (i + 1).ToString(); // Stt
+            texts[1].text = topPlayers[i].name;  // Tên
+            texts[2].text = topPlayers[i].score.ToString(); // Điểm
+
+            
+            Image background = row.GetComponent<Image>();
+            if (i == 0) background.color = new Color(1f, 0.84f, 0f); // Vàng
+            else if (i == 1) background.color = Color.gray; // Bạc
+            else if (i == 2) background.color = new Color(0.8f, 0.5f, 0.2f); // Đồng
+            else background.color = Color.gray; // Xám
+
+            Button button = row.GetComponent<Button>();
+            if (button != null)
+            {
+                ColorBlock colors = button.colors;
+                colors.highlightedColor = new Color(0.5f, 0.5f, 0.5f); // Xám đậm
+                button.colors = colors;
+            }
+        }
+
+        // Xếp hạng người chơi hiện tại
+        int rank = topPlayers.Length + 1;
+        for (int i = 0; i < topPlayers.Length; i++)
+        {
+            if (currentPlayerScore > topPlayers[i].score)
+            {
+                rank = i + 1;
+                break;
+            }
+        }
+        playerRankText.text = "Xếp hạng của bạn: " + rank;
+    }
+}
