18b9358d6e3e028f6236f46a6e604a6d78fcf13d
Ng.H.Chau <141216466+StelarNguyen@users.noreply.github.com>
2025-05-22 21:41:56 +0700

    Fix Menu and Camera, Add more level

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Đổi tên đúng theo tên scene của bạn
+    }
+}
+
